be878181e95902665d3608bb4e81d9d292a07ec5
Isaac Robbins <isaacprobbins@gmail.com>
2023-10-22 18:30:55 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { CabooseSDK } from '@caboosemedia/sdk'
 
-export class HelloWorld extends CabooseSDK {
+export default class HelloWorld extends CabooseSDK {
 
     public async start() {
         console.log('Hello, world!');
